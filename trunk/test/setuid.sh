e6c8cd01d59d9d701d8e76ed22287b76b97d394b
sueruthmarie <sueruthmarie@gmail.com>
2007-07-22 16:09:43 +0000

    Add Page Mill config

--- trunk/test/setuid.sh
+++ trunk/test/setuid.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # Run as root to setuid on programs in this directory that must
 # run with root permission
-chown root rdsniff rd3418
-chmod 4775 rdsniff rd3418
+chown root rdsniff rd3418 rd34182
+chmod 4775 rdsniff rd3418 rd34182
