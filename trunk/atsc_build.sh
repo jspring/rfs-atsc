cd6edbb384fe46f796515e074f2104d7d81eba3d
sueruthmarie <sueruthmarie@gmail.com>
2008-06-06 23:16:57 +0000

    Improved cleanup of build A    test/Makefile M    atsc_build.sh M    mkclean.sh

--- trunk/atsc_build.sh
+++ trunk/atsc_build.sh
@@ -1,20 +1,9 @@
 #!/bin/sh
 # simple script that builds ix code in the correct order
-cd sniffer
-echo sniffer
-make
-cd ../ab3418
-echo ab3418
-make
-cd ../ntcip
-echo ntipc
-make
-cd ../ix
-echo ix
-make
-cd ../phases
-echo phases
-make
-cd ../sendmsg2tlab
-echo sendmsg2tlab
-make
+for i in sniffer ab3418 ntcip ix phases sendmsg2tlab
+do
+	cd $i
+	make
+	make install
+	cd ..
+done
--- trunk/mkclean.sh
+++ trunk/mkclean.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-for i in ab3418 sniffer ntcip ix phases sendmsg2tlab 
+for i in ab3418 sniffer ntcip ix phases sendmsg2tlab test
 do
 	cd $i
 	make clean
