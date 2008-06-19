93103eb49bbba738f6d87da499e92e07091302b0
sueruthmarie <sueruthmarie@gmail.com>
2008-06-19 05:55:18 +0000

    Fixed non-SOBU case for SOBU Makefile. M    ix/Makefile M    atsc_build.sh M    phases/Makefile M    phase170/Makefile

--- trunk/atsc_build.sh
+++ trunk/atsc_build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # simple script that builds ix code in the correct order
-for i in sniffer ab3418 ntcip ix phases sendmsg2tlab
+for i in sniffer ab3418 ntcip ix phases phase170 sendmsg2tlab
 do
 	cd $i
 	make
