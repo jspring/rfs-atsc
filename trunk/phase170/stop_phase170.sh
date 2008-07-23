66702aa24ac6c6d17cadf2ac40ac8c24956038ce
Kun Zhou <kzhou@path.berkeley.edu>
2008-07-23 10:50:13 +0000

    add script to run phase170 standard alone

---
+++ trunk/phase170/stop_phase170.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# Stop the database queue processes and stop all priority programs
+killall -TERM phase170e
+killall -TERM read232
+killall -TERM veh_sig
+sleep 3
+killall -TERM db_slv
+sleep 1
+/home/tsp/test/db_clean.sh
---
+++ trunk/phase170/test_phase170.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# This scrpt runs the phase170 standard alone
+# For tsteing, phase170 does not updates the CICAS Traffic Signal database
+# However, the generated msg will be print on the screen
+/home/path/db/db_slv &
+sleep 1
+/home/tsp/test/veh_sig &
+sleep 1
+/home/tsp/test/read232 /dev/ttyS0 &
+sleep 1
+/home/atsc/test/phase170e -i 500 -v &
+
