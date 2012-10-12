e2d02795328c6364a648f030483da0398d1c2df9
jspring <jspring@path.berkeley.edu>
2012-10-12 16:37:06 -0700

    Moved contents of subversion "trunk" directory to top directory Removed trunk and tags directories

--- tags/Initial-0.0/test/stop_rfs.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-killall snd_ix_msg
-killall phases
-killall rdsniff_rfs
-killall rdntcip
-killall db_slv
--- tags/Initial-0.0/test/stop_timing.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-killall compare
-killall rdsniff_rfs
-killall rdntcip2
-killall db_slv
