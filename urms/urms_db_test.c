e251c98cda6defb35e37a6343bdfcc7beda3a8fa
John Spring <jspring@berkeley.edu>
2016-09-12 19:58:52 -0700

    modified:   urms_db_test.c Added db_var option for entering the base db variable. db_urms uses db_var_base + 2.

--- urms/urms_db_test.c
+++ urms/urms_db_test.c
@@ -12,6 +12,7 @@ extern int db_set_ramp_metering(db_clt_typ *pclt, db_urms_t *db_urms_msg);
 
 int main(int argc, char *argv[]) {
 	db_urms_t db_urms_msg;
+	int db_var = 3202;
 
         int option;
 
@@ -27,10 +28,10 @@ int main(int argc, char *argv[]) {
             xport, NULL, 0, NULL, 0)) == NULL){ 
             exit(EXIT_FAILURE);
 	}
-	db_clt_read(pclt, DB_URMS_VAR, sizeof(db_urms_t), &db_urms_msg);
 
+	db_clt_read(pclt, db_var, sizeof(db_urms_t), &db_urms_msg);
 
-        while ((option = getopt(argc, argv, "1:2:3:4:5:6:")) != EOF) {
+        while ((option = getopt(argc, argv, "1:2:3:4:5:6:7:8:9:A:B:C:d:")) != EOF) {
                 switch(option) {
                 case '1':
 			db_urms_msg.lane_1_release_rate = atoi(optarg);
@@ -50,12 +51,41 @@ int main(int argc, char *argv[]) {
                 case '6':
 			db_urms_msg.lane_2_plan = atoi(optarg);
                         break;
+                case '7':
+			db_urms_msg.lane_3_release_rate = atoi(optarg);
+                        break;
+                case '8':
+			db_urms_msg.lane_3_action = atoi(optarg);
+                        break;
+                case '9':
+			db_urms_msg.lane_4_plan = atoi(optarg);
+                        break;
+                case 'A':
+			db_urms_msg.lane_4_release_rate = atoi(optarg);
+                        break;
+                case 'B':
+			db_urms_msg.lane_4_action = atoi(optarg);
+                        break;
+                case 'C':
+			db_urms_msg.lane_3_plan = atoi(optarg);
+                        break;
+                case 'd':
+			db_var = atoi(optarg) + 2;
+                        break;
                 default:
                         printf("Usage: %s %s\n", argv[0], Usage);
                         exit(EXIT_FAILURE);
                         break;
                 }
         }
-
-	db_clt_write(pclt, DB_URMS_VAR, sizeof(db_urms_t), &db_urms_msg);
+	printf("db_var %d Lane 1: rate %d action %d plan %d Lane 2: rate %d action %d plan %d\n",
+		db_var,
+		db_urms_msg.lane_1_release_rate,
+		db_urms_msg.lane_1_action,
+		db_urms_msg.lane_1_plan,
+		db_urms_msg.lane_2_release_rate,
+		db_urms_msg.lane_2_action,
+		db_urms_msg.lane_2_plan
+	);
+	db_clt_write(pclt, db_var, sizeof(db_urms_t), &db_urms_msg);
 }
